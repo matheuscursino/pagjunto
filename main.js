9a6abd8e4779df5553d42b440f58362c845c805b
matheuscursino <matheuscursino115@gmail.com>
2025-06-01 11:09:22 -0300

    server now makes requests to the api to check if the partner or order exists

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 import express from 'express'
+import axios from 'axios'
 import bodyParser from 'body-parser'
 import dotenv  from 'dotenv'
 
 const app = express()
 const port = 80
+
+const axiosConfig = {
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}
 
 dotenv.config()
 
@@ -13,14 +20,31 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json())
 app.set('view engine', 'ejs')
-//app.set('views', './view')
 
 app.get('/', (req, res) =>  {
     res.send('index')
 })
 
-app.use('/:partnerId/:orderId', (req, res) => {
-    res.render('order', {partnerId: req.params.partnerId, orderId: req.params.orderId})
+app.get('/:partnerId/:orderId', (req, res) => {
+    axios.get('http://127.0.0.1:3000/partner', {
+        data: {
+            'partnerId': req.params.partnerId
+        }
+    }, axiosConfig).then((response) => {
+        axios.get('http://127.0.0.1:3000/order', {
+            data: {
+                'orderId': req.params.orderId
+            }
+        }, axiosConfig).then((response2) => {
+            console.log(response2)
+        }).catch((error2) => {
+            res.send("order doesnt exist")
+        })
+    }).catch((error) => {
+        res.send("partner doesnt exist")
+    })
+
+   // res.render('order', {partnerId: req.params.partnerId, orderId: req.params.orderId})
 })
 
 app.listen(port, () => {
